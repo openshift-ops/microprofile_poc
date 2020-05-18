329cabe3cf5bbd384b8731b466189422155cbcf6
Asus <Asus@DESKTOP-QA8MHVJ>
2020-05-18 22:31:13 +0530

    Thorntail Microprofile Project

---
+++ DemoThorntail/src/main/java/com/example/thorntail/DemoThorntail/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package com.example.thorntail.DemoThorntail.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
