576d8f0f57a5720646fe5c391f07e39b0e6fbe18
Asus <Asus@DESKTOP-QA8MHVJ>
2020-05-19 01:08:49 +0530

    Added @Produces and changed te ordering of annotations.

--- demo.wildfly.microprofile/src/main/java/com/example/microprofile/demo/wildfly/microprofile/HelloController.java
+++ demo.wildfly.microprofile/src/main/java/com/example/microprofile/demo/wildfly/microprofile/HelloController.java
@@ -1,18 +1,19 @@
 package com.example.microprofile.demo.wildfly.microprofile;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 /**
  *
  */
-@Path("/hello")
 @ApplicationScoped
+@Path("/hello")
 public class HelloController {
 
     @GET
+    @Produces("text/plain")
     public String sayHello() {
         return "Hello World";
     }
